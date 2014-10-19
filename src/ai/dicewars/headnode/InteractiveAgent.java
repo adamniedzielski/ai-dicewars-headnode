de8c5c6965cac8174256ad870bef37b978a12ba4
karlsss <karlsss@wp.pl>
2014-10-19 10:01:53 +0200

    printmap

--- src/ai/dicewars/headnode/InteractiveAgent.java
+++ src/ai/dicewars/headnode/InteractiveAgent.java
@@ -7,6 +7,7 @@ import ai.dicewars.common.Agent;
 import ai.dicewars.common.Answer;
 import ai.dicewars.common.Vertex;
 
+
 /*
  * TODO: this agent should display game state in the console
  * and then read move from command line
@@ -15,18 +16,33 @@ import ai.dicewars.common.Vertex;
 public class InteractiveAgent implements Agent {
 	
 	private int playerNumber;
+	private List<Vertex> vertices;
+	private ConcreteAnswer answer;
 
 	@Override
 	public Answer makeMove(List<Vertex> vertices) {
+		printmap(vertices);
 		System.out.println("Player " + playerNumber + " make move!");
 		Scanner in = new Scanner(System.in);
 		int notUsed = in.nextInt();
 		return new ConcreteAnswer(true, 0, 0);
+		
+		
 	}
 
 	@Override
 	public void setPlayerNumber(int playerNumber) {
 		this.playerNumber = playerNumber;
 	}
+	
+	public void printmap(List<Vertex> vertices)
+	{
+	for(int i=0; i<vertices.size(); i++)
+System.out.println(""+vertices.get(i).getId()+", "+ vertices.get(i).getNeighbours()+", "+vertices.get(i).getNumberOfDices()+" , "+vertices.get(i).getPlayer()+" "); 
+	
+	}
 
-}
+
+		
+	};
+
