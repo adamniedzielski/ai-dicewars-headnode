c50fa921e2cecd6f1738e534ef118389039f7510
Adam Niedzielski <adamsunday@gmail.com>
2014-10-28 00:06:17 +0100

    Create more complex map in FakeMapBuilder

--- src/ai/dicewars/headnode/FakeMapBuilder.java
+++ src/ai/dicewars/headnode/FakeMapBuilder.java
@@ -7,11 +7,12 @@ import java.util.List;
 public class FakeMapBuilder {
 	public List<ConcreteVertex> build() {
 		List<ConcreteVertex> vertices = new ArrayList<>();
-		ConcreteVertex first = new ConcreteVertex(1, Arrays.asList(2), 4, 0);
-		ConcreteVertex second = new ConcreteVertex(2, Arrays.asList(1), 6, 1);
-		vertices.add(first);
-		vertices.add(second);
-
+		vertices.add(new ConcreteVertex(1, Arrays.asList(2), 4, 0));
+		vertices.add(new ConcreteVertex(2, Arrays.asList(1, 3, 4), 1, 0));
+		vertices.add(new ConcreteVertex(3, Arrays.asList(5, 2), 2, 1));
+		vertices.add(new ConcreteVertex(4, Arrays.asList(2, 6, 5), 2, 1));
+		vertices.add(new ConcreteVertex(5, Arrays.asList(3, 4, 6), 2, 1));
+		vertices.add(new ConcreteVertex(6, Arrays.asList(4, 5), 3, 0));
 		return vertices;
 	}
 }
